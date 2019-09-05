60d7722c56d644d8aff38db3ff91bf3ee55cde98
tedlaz <tedlaz@gmail.com>
2019-09-05 13:29:29 +0300

    first weblinks commit

---
+++ sidemenu.js
@@ -0,0 +1,29 @@
+document.write('\
+<ul class="sidebar-nav">\
+<li><a href="index.html">News</a></li>\
+<li><a href="downloads.html">Downloads</a></li>\
+<li><a href="sintv.html">Σινεμά-Τηλεόραση</a></li>\
+<li><a href="stores.html">Καταστήματα</a></li>\
+<li><a href="search.html">Αναζητήσεις - Χάρτες</a></li>\
+<li><a href="magazines.html">Περιοδικά</a></li>\
+<li><a href="trips.html">Ταξίδια</a></li>\
+<li><a href="music.html">Μουσική</a></li>\
+<li><a href="tax.html">Φορολογικά-Ασφαλιστικά</a></li>\
+<li><a href="education.html">Εκπαιδευτικά</a></li>\
+<li><a href="windows.html">Windows</a></li>\
+<li><a href="android.html">Android</a></li>\
+<li><a href="linux.html">Linux</a></li>\
+<li><a href="soft-server.html">Server software</a></li>\
+<li><a href="soft-media.html">Multimedia software</a></li>\
+<li><a href="soft-office.html">Office-Utils software</a></li>\
+<li><a href="soft-web.html">Web software</a></li>\
+<li><a href="development.html">Development</a></li>\
+<li><a href="neuralnet.html">Machine Learning</a></li>\
+<li><a href="webserv.html">Web Υπηρεσίες</a></li>\
+<li><a href="personal.html">Προσωπικά</a></li>\
+<li><a href="youtube.html">YouTube Channels</a></li>\
+<li><a href="hardware.html">Hardware</a></li>\
+<li><a href="sequrity.html">Sequrity - ip tools</a></li>\
+<li><a href="games.html">Games</a></li>\
+</ul>\
+');
